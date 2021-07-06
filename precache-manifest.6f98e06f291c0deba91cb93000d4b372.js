54b4a1123f3ed17f12fa1296b64862b1f09b4a85
vinayak <itssvinayak@gmail.com>
2021-07-06 23:00:55 +0530

    Updates

---
+++ precache-manifest.6f98e06f291c0deba91cb93000d4b372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d84bfd71144312eee3b28f58ec225a",
+    "url": "/QuizOn/index.html"
+  },
+  {
+    "revision": "adea984d877a906a9fff",
+    "url": "/QuizOn/static/css/main.febdf5dc.chunk.css"
+  },
+  {
+    "revision": "435749a88b13cf8cb1f8",
+    "url": "/QuizOn/static/js/2.d7458e4b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/QuizOn/static/js/2.d7458e4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adea984d877a906a9fff",
+    "url": "/QuizOn/static/js/main.bb07046e.chunk.js"
+  },
+  {
+    "revision": "2e154f7540e10dd4b2cd",
+    "url": "/QuizOn/static/js/runtime-main.6366b0a7.js"
+  }
+]);
